771e28ce5d7af3a145ebc07d87b98e042f8be0db
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-12 13:55:15 +0800

    implement responsiveness to hero section of landing

--- coc-website/app/page.tsx
+++ coc-website/app/page.tsx
@@ -10,54 +10,55 @@ export default function Hero() {
     <section className="relative w-full flex flex-col items-center bg-white">
 
       {/* --- PART 1: GRAY BACKGROUND SECTION --- */}
-      <div className="relative h-204 w-360 bg-white pt-30 px-4 flex flex-col items-center text-center gap-10 rounded-b-[40px] overflow-hidden">
+      <div className="relative h-112 md:h-204 w-full md:w-360 bg-white pt-20 md:pt-30 px-4 flex flex-col items-center text-center gap-10 rounded-b-[40px] overflow-hidden">
 
-        {/* DECORATIVE ELLIPSES (Positioned absolute bottom-left/right of this gray box) */}
+        {/* DECORATIVE ELLIPSES */}
         <div className="absolute bottom-0 left-0 z-0 pointer-events-none">
           <Image
-            src="/hero-images/ellipse1.svg"
+            src="/hero-images/ellipse3.svg"
             alt="Gradient Blur Left"
-            width={900}
+            width={1000}
             height={1000}
-            className="object-contain"
+            className="object-contain opacity-60 md:opacity-100"
           />
         </div>
         <div className="absolute bottom-0 right-0 z-0 pointer-events-none">
           <Image
             src="/hero-images/ellipse2.svg"
             alt="Gradient Blur Right"
-            width={900}
+            width={1000}
             height={1000}
-            className="object-contain"
+            className="object-contain opacity-60 md:opacity-100"
           />
         </div>
 
-        {/* TEXT CONTENT (z-10 to sit above ellipses) */}
+        {/* TEXT CONTENT */}
         <div className="relative z-10 space-y-6 max-w-5xl">
-          <h1 className={`${montserrat.className} text-8xl text-brand-brown font-medium leading-tight`}>
-            <span className="block whitespace-nowrap">Strong Coffee.</span>
-            <span className="block whitespace-nowrap">Strong Opinions.</span>
+          <h1 className={`${montserrat.className} text-4xl md:text-8xl text-brand-brown font-normal leading-tight`}>
+            <span className="block md:whitespace-nowrap">Strong Coffee.</span>
+            <span className="block md:whitespace-nowrap">Strong Opinions.</span>
           </h1>
 
-          <button className="bg-accent-brown hover:bg-[#8e6a45] text-white px-7 pt-4 pb-5 rounded-[15px] text-2xl font-medium transition-colors duration-200">
+          <button className={`${montserrat.className} bg-brand-green cursor-pointer text-white px-5 pt-2 pb-3 md:px-7 md:pt-4 md:pb-5 rounded-[15px] text-md md:text-2xl font-medium transition-colors duration-200`}>
             Read an article
           </button>
         </div>
       </div>
 
       {/* --- PART 2: THE CARD --- */}
-      <div className="relative z-20 -mt-80 w-300">
+       <div className="relative z-20 -mt-35 md:-mt-80 w-[90%] md:w-300 pb-10 md:pb-0">
+        
         {/* Outer Grayish Transparent Border Wrapper */}
-        <div className="bg-[#DFDADA69] w-full p-8 rounded-[25px] backdrop-blur-md">
+        <div className="bg-[#DFDADA69] w-full p-3 md:p-8 rounded-[25px] backdrop-blur-md">
 
           {/* Inner White Card */}
-          <div className="w-full bg-white backdrop-blur-md p-20 rounded-[25px]">
+          <div className="w-full bg-white backdrop-blur-md p-4 md:p-20 rounded-[25px]">
 
             {/* Grid Layout */}
-            <div className="grid grid-cols-2 gap-4 h-150">
+            <div className="grid grid-cols-2 gap-2 md:gap-4 h-80 md:h-150">
 
               {/* Left Column Images */}
-              <div className="flex flex-col gap-4 h-full">
+              <div className="flex flex-col gap-2 md:gap-4 h-full">
                 {/* Large Top */}
                 <div className="relative w-full flex-3 min-h-0 overflow-hidden rounded-2xl">
                   <Image
@@ -79,7 +80,7 @@ export default function Hero() {
               </div>
 
               {/* Right Column Images */}
-              <div className="flex flex-col gap-4 h-full">
+              <div className="flex flex-col gap-2 md:gap-4 h-full">
                 {/* Small Top */}
                 <div className="relative w-full flex-1 min-h-0 overflow-hidden rounded-2xl">
                   <Image
